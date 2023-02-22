ed9eafb2db3478b2c078942919be6452004fcdab
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-02-22 08:20:00 +0300

    feat: implement two tests

--- src/tests/ListItem.test.jsx
+++ src/tests/ListItem.test.jsx
@@ -47,10 +47,30 @@ describe('ListItem', () => {
     });
     
     //TODO: implement this
-    it('callback is called', () => {});
+    it('callback is called', () => {
+        const { getByTestId } = render(
+            <ListItem
+                id='list-item-1'
+                checkable={true}
+                onCheck={mockOnCheck}
+                item='Lorem ipsum dolor sit amet consectetur'
+            />
+        );
+        expect(mockOnCheck.mock.calls).toHaveLength(1);
+    });
 
     //TODO: implement this
-    it('callback is not called when not checkable', () => {});
+    it('callback is not called when not checkable', () => {
+        const { getByTestId } = render(
+            <ListItem
+                id='list-item-1'
+                checkable={false}
+                onCheck={mockOnCheck}
+                item='Lorem ipsum dolor sit amet consectetur'
+            />
+        );
+        expect(mockOnCheck.mock.calls).toHaveLength(1);
+    });
 
     //TODO: implement this
     it('matches saved snapshot', () => {
